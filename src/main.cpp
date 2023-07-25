d73f09cba3731641a3d1eddb04c501567c0aea65
Kravcik <29161621k@gmail.com>
2023-07-25 15:21:24 +0200

    removed final classification of testing data

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,10 @@ const size_t BATCH_SIZE = 200;
 const int EVALUATION_SET_SIZE_PERCENTAGE = 10;
 const double BIAS_INIT_VALUE = 0;
 const double WEIGHT_DECAY = 0.01;
+const int NORMALIZATION_FACTOR = 255;
 const string OPTIMIZER = "RMS"; // TODO: use enums instead of strings
 
 double learningRate = 0.001;
-size_t epochs = 70;
 int threads = 8;
 
 //------------------------------------------------------------
@@ -30,14 +30,11 @@ int threads = 8;
 int main(int argc, char** argv)
 {
 	srand(time(NULL));
-	omp_set_num_threads(threads);
 
 	if (argc > 2) { // default values are used if no arguments are passed
-		epochs = stoi(argv[1]);
-		threads = stoi(argv[2]);
+		threads = stoi(argv[1]);
 	}
-
-	cout << "Epochs: " << epochs << endl;
+	omp_set_num_threads(threads);
 
 	cout << "Architecture: [";
 	for (auto val : HIDDEN_LAYERS_NEURON_COUNT) {
@@ -46,10 +43,8 @@ int main(int argc, char** argv)
 	cout << OUTPUT_LAYER_SIZE << "] ";
 	cout << " Learning rate: " << learningRate << " Batch size: " << BATCH_SIZE << " decay: " << WEIGHT_DECAY << endl;
 
-	Matrix trainDataVectors = loadFromCSV("data/fashion_mnist_train_vectors.csv", 255); // dividing by 255 is done to normalize the data
+	Matrix trainDataVectors = loadFromCSV("data/fashion_mnist_train_vectors.csv", NORMALIZATION_FACTOR); // dividing by NORMALIZATION_FACTOR
 	Matrix trainDataLabels = loadFromCSV("data/fashion_mnist_train_labels.csv");
-	Matrix testDataVectors = loadFromCSV("data/fashion_mnist_test_vectors.csv", 255); // dividing by 255 is done to normalize the data
-	Matrix testDataLabels = loadFromCSV("data/fashion_mnist_test_labels.csv");
 
 	size_t trainDataRows = trainDataVectors.data.size();
 	size_t trainDataCols = trainDataVectors.data[0].size();
@@ -88,19 +83,19 @@ int main(int argc, char** argv)
 
 	size_t epochCounter = 0;
 
-	while (accuracy < 0.89) {
+	while (true) {
 
 		cout << "Epoch: " << epochCounter << endl;
 
 		error = evaluateNetworkError(trainDataVectors, trainDataLabels, weights, nonStaticNeuronLayersPotentials, nonStaticNeuronLayersOutputs, ACT_ReLU, evaluationOffset);
 		accuracy = evaluateNetworkAccuracy(trainDataVectors, trainDataLabels, weights, nonStaticNeuronLayersPotentials, nonStaticNeuronLayersOutputs, ACT_ReLU, evaluationOffset);
 
-		if (accuracy > 0.86) { // adaptive learning rate
-			learningRate = 0.0001;
-		}
-		else if (accuracy > 0.85) {
-			learningRate = 0.0005;
-		}
+		//if (accuracy > 0.86) { // adaptive learning rate
+		//	learningRate = 0.0001;
+		//}
+		//else if (accuracy > 0.85) {
+		//	learningRate = 0.0005;
+		//}
 
 		auto epochLength = duration_cast<milliseconds>(epochTimerEnd - epochTimerStart); // measuring time spent on epoch
 		auto runTimeMs = duration_cast<milliseconds>(epochTimerEnd - runStart); // measuring total time spent on training
@@ -110,7 +105,6 @@ int main(int argc, char** argv)
 		cout << "Accuracy: " << accuracy << ", Error: " << error << ", Runtime: " << runTimeMins << " min., ms since last epoch: " << epochLength.count() << endl;
 
 		for (size_t trainingExampleOffset = 0; trainingExampleOffset + BATCH_SIZE <= evaluationOffset; trainingExampleOffset += BATCH_SIZE) {
-
 			for (Matrix& layer : weightChangeSum){ // reset before each batch
 				for (vector<double>& v1 : layer.data){
 					fill(v1.begin(), v1.end(), 0);
@@ -130,15 +124,5 @@ int main(int argc, char** argv)
 		epochTimerEnd = high_resolution_clock::now();
 		epochCounter += 1;
 	}
-
-	vector<double> outLabels;
-
-	for (auto& inputVector : testDataVectors.data) {
-		forwardPass(inputVector, weights, nonStaticNeuronLayersPotentials, nonStaticNeuronLayersOutputs, ACT_ReLU);
-		outLabels.push_back(vecToScalar(nonStaticNeuronLayersOutputs.back()));
-	}
-
-	writeToFile("actualPredictions", outLabels);
-
 	return 0;
 }
